06b518fc5655f376233ab738d87c787509f5f07b
gowthamtraining <gouthamvenkatesh21@gmail.com>
2023-08-23 16:38:16 +0530

    first commit

--- src/componets/Dropdown.jsx
+++ src/componets/Dropdown.jsx
@@ -2,28 +2,43 @@ import React, { useState } from 'react'
 
 const Dropdown = () => {
     const[selectValue,setSelectvalue] = useState("")
+    const [formData,setFormdata] = useState({fullName:"",language:"",hobbies:["football"],gender:"male"})
     const[text,setText] = useState("")
     const [value,setValue] = useState("")
-    const [clicked,setClicked] = useState(false)
-    const [radio,setRadio]= useState(false)
     function handledropdownchange(e){
         setSelectvalue(e.target.value)
+        if(formData.language === e.target.value){
+          setFormdata({...formData,language:""})
+        }
+        else{
+          setFormdata({...formData,language:e.target.value})
+        }
     }
-    function handleCheckbpx(){
-      setClicked(!clicked)
+    function handleCheckbpx(e){
+      console.log(e.target.name)
+      let array = [...formData.hobbies]
+      if(formData.hobbies.includes(e.target.name)){
+        array = array.filter((item)=>item!==e.target.name)
+      }
+      else{
+        array.push(e.target.name)
+      }
+      setFormdata({...formData,hobbies:array})
     }
-    function handleRadio(){
-      setRadio(!radio)
+    function handleRadio(e){
+      if(formData.gender === e.target.name){
+        setFormdata({...formData,gender :""})
+      }
+      else{
+        setFormdata({...formData,gender:e.target.name})
+      }
     }
     function handletext(e){
       setText(e.target.value)
     }
-    function handlesubmit(){
+    function handlesubmit(e){
         if(selectValue === ""){
            alert("kindly select") 
-        }
-        if(clicked === false || radio === false){
-          alert("kindly clicked the ckecbox or radio buttons")
         }
         if(text === ""){
           setValue("kindly select text box")
@@ -32,26 +47,34 @@ const Dropdown = () => {
           console.log(selectValue)
         }
     }
-    console.log(clicked)
-    console.log(radio)
-    console.log(text)
+    console.log(formData)
+    function printfullname(e){
+      if(e.key === "Enter"){
+        setFormdata({...formData,fullName:text})
+        setText("")
+      }
+    }
   return (
     <div>
     <button onClick={handlesubmit}>submit</button>
       <select value={selectValue} onChange={handledropdownchange} >
-        <option value="heloo">heloo</option>
-        <option value="home" >home</option>
-        <option value="service">service</option>
-        <option value="footer">footer</option>
-        <option value="blog">blog</option>
-        <option value="about">about</option>
+        <option value="hindi">hindi</option>
+        <option value="english" >english</option>
+        <option value="tamil">tamil</option>
+        <option value="telugu">telugu</option>
       </select>
-      <input type="text" name="" id="" value={text} onChange={handletext}/>
-      <input type='checkbox' checked={clicked} onChange={handleCheckbpx}></input>
-      <input type="radio" checked={radio} onChange={handleRadio}/>
+      <label id='fullName'>fullName:</label>
+      <input type="text" name="fullName" id="" value={text} onChange={handletext} onKeyDown={printfullname}/>
+      <label htmlFor="football">football</label>
+      <input type='checkbox' checked={formData.hobbies.includes("football")} onChange={handleCheckbpx} name='football'></input>
+      <label htmlFor="handball">handball</label>
+      <input type='checkbox' checked={formData.hobbies.includes("handball")} onChange={handleCheckbpx} name='handball'></input>
+      <label htmlFor="male">male:</label>
+      <input type="radio" checked={formData.gender === "male"} onChange={handleRadio} name='male'/>
+      <label htmlFor="female">female</label>
+      <input type="radio" checked = {formData.gender === "female"}  onChange={handleRadio} name='female'/>
       <p>{value}</p>
     </div>
   )
 }
-
 export default Dropdown
